ae2d65f05997a7ed719e2341c6d937defccd39f5
fanmiao11 <miao623@outlook.com>
2022-06-27 21:11:43 +0800

    处理less文件

--- src/main.js
+++ src/main.js
@@ -7,4 +7,6 @@ import $ from 'jquery'
 $('#swiper').css('background-color', 'red');
 
 // 引入css文件
-import './styles/index.css'
+// import './styles/index.css'
+// 引入less文件
+import './styles/index.less';
